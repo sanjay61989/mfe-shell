f893ea8b897d9328e52a3552e360541fc9cc5069
Sanjay Chhabria <sonu61989@gmail.com>
2024-10-29 09:05:17 +0530

    Added dockker and nginx

---
+++ DockerStep.ps1
@@ -0,0 +1,17 @@
+# Variables for image and container names
+$imageName = "my-web-app"
+$imageTag = "latest"
+$containerName = "my-web-app-container"
+
+# Build the Docker image
+docker build --progress=plain -t "${imageName}:${imageTag}" .
+
+# Stop and remove the container if it's running
+$containerId = docker ps -q --filter "name=$containerName"
+if ($containerId) {
+    docker stop $containerId
+    docker rm $containerId
+}
+
+# Start a new container from the image
+docker run -d -p 8080:80 --name $containerName "${imageName}:${imageTag}"
